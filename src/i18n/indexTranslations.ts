152797ab266d6581cbd4832b0ab5b1ad78ce00b6
Pau Segués Vitutia <psv.segues@gmail.com>
2025-11-02 19:24:06 +0100

    improving languages with external documents

---
+++ src/i18n/indexTranslations.ts
@@ -0,0 +1,20 @@
+export const indexTranslations = {
+    'ca': {
+        'pageTitle': 'Pau Segués Vitutia',
+        'pageDescription': 'Pau Segués Vitutia Web',
+
+        'HeroSubtitle': 'Desenvolupador Web',
+    },
+    'es': {
+        'pageTitle': 'Pau Segués Vitutia',
+        'pageDescription': 'Pau Segués Vitutia Web',
+
+        'HeroSubtitle': 'Programador Web',
+    },
+    'en': {
+        'pageTitle': 'Pau Segués Vitutia',
+        'pageDescription': 'Pau Segués Vitutia Web',
+
+        'HeroSubtitle': 'Web Developer',
+    },
+}
---
+++ src/i18n/layoutTranslations.ts
@@ -0,0 +1,23 @@
+export const LayoutTranslations = {
+    'ca': {
+        'headerLinkTexts': ['Home', 'Sobre Mi', 'Projectes', 'Contacte'],
+        'headerLinksHref': ['/', '', '', ''],
+        
+        'footerLegal': ["Avis Legal", ""],
+        'footerPrivacity': ["Política de Privacitat", ""],
+    },
+    'es': {
+        'headerLinkTexts': ['Home', 'Sobre Mi', 'Proyectos', 'Contacto'],
+        'headerLinksHref': ['/es/', '', '', ''],
+        
+        'footerLegal': ["Aviso Legal", ""],
+        'footerPrivacity': ["Política de privacidad", ""],
+    },
+    'en': {
+        'headerLinkTexts': ['Home', 'About Me', 'Projects', 'Contact'],
+        'headerLinksHref': ['/en/', '', '', ''],
+        
+        'footerLegal': ["Legal Notice", ""],
+        'footerPrivacity': ["Privacy policy", ""],
+    }
+}
