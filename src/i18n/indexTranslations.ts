822ef98b80bb5a217941684c5ed1d7e43aba46d1
Pau Segués Vitutia <psv.segues@gmail.com>
2025-11-02 21:00:08 +0100

    starting about me summary on index

--- src/i18n/indexTranslations.ts
+++ src/i18n/indexTranslations.ts
@@ -4,17 +4,32 @@ export const indexTranslations = {
         'pageDescription': 'Pau Segués Vitutia Web',
 
         'HeroSubtitle': 'Desenvolupador Web',
+
+        'AboutTitle': 'Sobre Mi',
+        'AboutLine01': '24 de Juliol del 2005 [ 20 Anys ]',
+        'AboutLine02': 'Solsona, Solsonès, Catalunya',
+        'AboutLine03': "Estudiant Desenvolupament d'aplicacions Web",
     },
     'es': {
         'pageTitle': 'Pau Segués Vitutia',
         'pageDescription': 'Pau Segués Vitutia Web',
 
         'HeroSubtitle': 'Programador Web',
+
+        'AboutTitle': 'Sombre Mí',
+        'AboutLine01': '24 de Julio del 2005 [ 20 años ]',
+        'AboutLine02': 'Solsona, Solsonés, Cataluña',
+        'AboutLine03': "Estudiando Desenvolupamiento d'aplicaciones Web",
     },
     'en': {
         'pageTitle': 'Pau Segués Vitutia',
         'pageDescription': 'Pau Segués Vitutia Web',
 
         'HeroSubtitle': 'Web Developer',
+
+        'AboutTitle': 'About Me',
+        'AboutLine01': '24th July 2005 [ 20 years old ]',
+        'AboutLine02': 'Solsona, Catalonia',
+        'AboutLine03': 'Studying Web Application Development',
     },
 }
