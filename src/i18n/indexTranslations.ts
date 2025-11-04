16a7edb6238e58be75d863171571a1c57d87d045
PaAu5 <psv.segues@gmail.com>
2025-11-04 16:57:04 +0100

    add a first version of about me summary

--- src/i18n/indexTranslations.ts
+++ src/i18n/indexTranslations.ts
@@ -6,9 +6,7 @@ export const indexTranslations = {
         'HeroSubtitle': 'Desenvolupador Web',
 
         'AboutTitle': 'Sobre Mi',
-        'AboutLine01': '24 de Juliol del 2005 [ 20 Anys ]',
-        'AboutLine02': 'Solsona, Solsonès, Catalunya',
-        'AboutLine03': "Estudiant Desenvolupament d'aplicacions Web",
+        'AboutText': "Hola, sóc el Pau Segués Vitutia. Estudiant de Desenvolupament d’Aplicacions Web a Lacetània (Manresa). M’apassiona la tecnologia i la programació, i m'agrada buscar i crear solucions digitals funcionals i creatives.",
     },
     'es': {
         'pageTitle': 'Pau Segués Vitutia',
@@ -17,9 +15,7 @@ export const indexTranslations = {
         'HeroSubtitle': 'Programador Web',
 
         'AboutTitle': 'Sombre Mí',
-        'AboutLine01': '24 de Julio del 2005 [ 20 años ]',
-        'AboutLine02': 'Solsona, Solsonés, Cataluña',
-        'AboutLine03': "Estudiando Desenvolupamiento d'aplicaciones Web",
+        'AboutText': 'Hola, soy Pau Segués Vitutia. Estudiante de Desarrollo de Aplicaciones Web en Lacetània (Manresa). Me apasiona la tecnología y la programación, y me gusta buscar y crear soluciones digitales funcionales y creativas.',
     },
     'en': {
         'pageTitle': 'Pau Segués Vitutia',
@@ -28,8 +24,6 @@ export const indexTranslations = {
         'HeroSubtitle': 'Web Developer',
 
         'AboutTitle': 'About Me',
-        'AboutLine01': '24th July 2005 [ 20 years old ]',
-        'AboutLine02': 'Solsona, Catalonia',
-        'AboutLine03': 'Studying Web Application Development',
+        'AboutText': 'Hi, I’m Pau Segués Vitutia. A Web Application Development student at Lacetània (Manresa). I’m passionate about technology and programming, and I enjoy designing and creating functional, creative digital solutions.',
     },
 }
