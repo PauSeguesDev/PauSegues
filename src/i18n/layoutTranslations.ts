67c888e104771e0e8f7598882a47205e5bf057ee
PaAu5 <psv.segues@gmail.com>
2025-11-12 19:46:09 +0100

    footer creation

--- src/i18n/layoutTranslations.ts
+++ src/i18n/layoutTranslations.ts
@@ -3,6 +3,8 @@ export const LayoutTranslations = {
         'headerLinkTexts': ['Home', 'Sobre Mi', 'Projectes', 'Contacte'],
         'headerLinksHref': ['/', '', '', ''],
         
+        'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        'titleSocial': "Les meves xarxes socials:",
         'footerLegal': ["Avis Legal", ""],
         'footerPrivacity': ["Política de Privacitat", ""],
         'footerCookies': ["Política de Cookies", ""],
@@ -11,6 +13,8 @@ export const LayoutTranslations = {
         'headerLinkTexts': ['Home', 'Sobre Mi', 'Proyectos', 'Contacto'],
         'headerLinksHref': ['/es/', '', '', ''],
         
+        'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        'titleSocial': "Mis redes sociales:",
         'footerLegal': ["Aviso Legal", ""],
         'footerPrivacity': ["Política de privacidad", ""],
         'footerCookies': ["Política de cookies", ""],
@@ -19,6 +23,8 @@ export const LayoutTranslations = {
         'headerLinkTexts': ['Home', 'About Me', 'Projects', 'Contact'],
         'headerLinksHref': ['/en/', '', '', ''],
         
+        'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        'titleSocial': "My social networks:",
         'footerLegal': ["Legal Notice", ""],
         'footerPrivacity': ["Privacy policy", ""],
         'footerCookies': ["Cookie policy", ""],
