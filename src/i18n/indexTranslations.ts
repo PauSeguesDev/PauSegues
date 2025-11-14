56e48e228001a14bcb5916f1736a1fd927aa94cc
PaAu5 <psv.segues@gmail.com>
2025-11-14 16:20:37 +0100

    improving index adding the last projects

--- src/i18n/indexTranslations.ts
+++ src/i18n/indexTranslations.ts
@@ -7,6 +7,9 @@ export const indexTranslations = {
 
         'AboutTitle': 'Sobre Mi',
         'AboutText': "Hola, sóc el Pau Segués Vitutia. Estudiant de Desenvolupament d’Aplicacions Web a Lacetània (Manresa). M’apassiona la tecnologia i la programació, i m'agrada buscar i crear solucions digitals funcionals i creatives.",
+        
+        'LastsProjectsTitle': "Últims Projectes",
+        'LastsProjectsButtonText': "Veure projecte"
     },
     'es': {
         'pageTitle': 'Pau Segués Vitutia',
@@ -16,6 +19,9 @@ export const indexTranslations = {
 
         'AboutTitle': 'Sombre Mí',
         'AboutText': 'Hola, soy Pau Segués Vitutia. Estudiante de Desarrollo de Aplicaciones Web en Lacetània (Manresa). Me apasiona la tecnología y la programación, y me gusta buscar y crear soluciones digitales funcionales y creativas.',
+    
+        'LastsProjectsTitle': "Últimos Proyectos",
+        'LastsProjectsButtonText': "Ver proyecto"
     },
     'en': {
         'pageTitle': 'Pau Segués Vitutia',
@@ -25,5 +31,8 @@ export const indexTranslations = {
 
         'AboutTitle': 'About Me',
         'AboutText': 'Hi, I’m Pau Segués Vitutia. A Web Application Development student at Lacetània (Manresa). I’m passionate about technology and programming, and I enjoy designing and creating functional, creative digital solutions.',
+    
+        'LastsProjectsTitle': "Lasts Projects",
+        'LastsProjectsButtonText': "See project"
     },
 }
