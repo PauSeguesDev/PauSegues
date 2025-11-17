66d6201903f5f5502f658fd563fc2bb990702cd9
PaAu5 <psv.segues@gmail.com>
2025-11-17 09:34:42 +0100

    Creation of About, Projects Pages. Improving Buttons and project card components. Creating Inner Title Section, Text-Image Section.

---
+++ src/i18n/aboutTranslations.ts
@@ -0,0 +1,62 @@
+export const aboutTranslations = {
+    'ca': {
+        'pageTitle': 'Sobre Mi',
+        'pageDescription': 'Aquest soc jo, Pau Segués Vitutia.',
+
+        'title': 'Sobre Mi',
+        'path': [
+            { name: 'Home', href: '/' },
+            { name: 'About', href: '/about-me' },
+        ],
+
+        subtitle: 'Aquest soc jo',
+        content: [
+            'Lorem ipsum dolor sit amet consectetur adipiscing elit turpis mus laoreet, senectus montes netus in cubilia eleifend primis posuere aliquam id dictumst, consequat cum egestas hendrerit fringilla platea condimentum est cras. Praesent ornare aptent volutpat platea vulputate condimentum a feugiat commodo, varius rhoncus interdum penatibus porta suscipit tellus ut, morbi est quisque id litora vel orci pulvinar. Fringilla duis tortor senectus porttitor fames mi cum placerat justo suscipit hac molestie non dapibus, volutpat mollis quisque posuere tincidunt cursus habitasse ornare donec libero enim lacus taciti, magna netus augue aliquam diam inceptos ac elementum ultricies convallis montes venenatis suspendisse.',
+            'Tincidunt et tempor pharetra nisl interdum nibh viverra eleifend ridiculus, risus tempus neque lobortis bibendum sodales dictum natoque nunc, porta ultrices faucibus lacinia litora parturient ac conubia. Neque eget quam rutrum pulvinar donec euismod egestas mus, ligula facilisi interdum fusce aptent senectus pellentesque, rhoncus proin suspendisse ante montes aliquam est. Sagittis netus curae risus aliquam libero sociosqu, auctor egestas quisque interdum pulvinar, ante scelerisque nostra convallis nulla. Vehicula interdum in montes maecenas sem sed vitae rutrum, proin venenatis facilisis platea fermentum lacinia suspendisse condimentum, dapibus hendrerit curabitur viverra urna erat semper.',
+        ],
+        image: {
+            src: '/images/pau-segues.webp',
+            alt: 'Sobre Mi',
+        }
+    },
+    'es': {
+        'pageTitle': 'Sobre Mi',
+        'pageDescription': 'Soy yo, Pau Segués Vitutia.',
+
+        'title': 'Sobre Mi',
+        'path': [
+            { name: 'Home', href: '/es/' },
+            { name: 'About', href: '/es/about-me' },
+        ],
+
+        subtitle: 'Éste soy yo',
+        content: [
+            'Lorem ipsum dolor sit amet consectetur adipiscing elit turpis mus laoreet, senectus montes netus in cubilia eleifend primis posuere aliquam id dictumst, consequat cum egestas hendrerit fringilla platea condimentum est cras. Praesent ornare aptent volutpat platea vulputate condimentum a feugiat commodo, varius rhoncus interdum penatibus porta suscipit tellus ut, morbi est quisque id litora vel orci pulvinar. Fringilla duis tortor senectus porttitor fames mi cum placerat justo suscipit hac molestie non dapibus, volutpat mollis quisque posuere tincidunt cursus habitasse ornare donec libero enim lacus taciti, magna netus augue aliquam diam inceptos ac elementum ultricies convallis montes venenatis suspendisse.',
+            'Tincidunt et tempor pharetra nisl interdum nibh viverra eleifend ridiculus, risus tempus neque lobortis bibendum sodales dictum natoque nunc, porta ultrices faucibus lacinia litora parturient ac conubia. Neque eget quam rutrum pulvinar donec euismod egestas mus, ligula facilisi interdum fusce aptent senectus pellentesque, rhoncus proin suspendisse ante montes aliquam est. Sagittis netus curae risus aliquam libero sociosqu, auctor egestas quisque interdum pulvinar, ante scelerisque nostra convallis nulla. Vehicula interdum in montes maecenas sem sed vitae rutrum, proin venenatis facilisis platea fermentum lacinia suspendisse condimentum, dapibus hendrerit curabitur viverra urna erat semper.',
+        ],
+        image: {
+            src: '/images/pau-segues.webp',
+            alt: 'Sobre Mi',
+        }
+    },
+    'en': {
+        'pageTitle': 'About Me',
+        'pageDescription': 'I am Pau Segués Vitutia.',
+
+        'title': 'About Me',
+        'path': [
+            { name: 'Home', href: '/en/' },
+            { name: 'About', href: '/en/about-me' },
+        ],
+
+        subtitle: 'I am Pau Segués Vitutia',
+        content: [
+            'Lorem ipsum dolor sit amet consectetur adipiscing elit turpis mus laoreet, senectus montes netus in cubilia eleifend primis posuere aliquam id dictumst, consequat cum egestas hendrerit fringilla platea condimentum est cras. Praesent ornare aptent volutpat platea vulputate condimentum a feugiat commodo, varius rhoncus interdum penatibus porta suscipit tellus ut, morbi est quisque id litora vel orci pulvinar. Fringilla duis tortor senectus porttitor fames mi cum placerat justo suscipit hac molestie non dapibus, volutpat mollis quisque posuere tincidunt cursus habitasse ornare donec libero enim lacus taciti, magna netus augue aliquam diam inceptos ac elementum ultricies convallis montes venenatis suspendisse.',
+            'Tincidunt et tempor pharetra nisl interdum nibh viverra eleifend ridiculus, risus tempus neque lobortis bibendum sodales dictum natoque nunc, porta ultrices faucibus lacinia litora parturient ac conubia. Neque eget quam rutrum pulvinar donec euismod egestas mus, ligula facilisi interdum fusce aptent senectus pellentesque, rhoncus proin suspendisse ante montes aliquam est. Sagittis netus curae risus aliquam libero sociosqu, auctor egestas quisque interdum pulvinar, ante scelerisque nostra convallis nulla. Vehicula interdum in montes maecenas sem sed vitae rutrum, proin venenatis facilisis platea fermentum lacinia suspendisse condimentum, dapibus hendrerit curabitur viverra urna erat semper.',
+        ],
+        image: {
+            src: '/images/pau-segues.webp',
+            alt: 'About Me',
+        }
+    }
+}
---
+++ src/i18n/projectsTranslations.ts
@@ -0,0 +1,41 @@
+export const projectsTranslations = {
+    'ca': {
+        'pageTitle': 'Projectes',
+        'pageDescription': 'Aquesta és la meva llista de projectes.',
+
+        'title': 'Projectes',
+        'path': [
+            { name: 'Home', href: '/' },
+            { name: 'Projectes', href: '/projects' },
+        ],
+
+        'subtitle': 'Tots els Projectes',
+        'buttonText': 'Veure projecte'
+    },
+    'es': {
+        'pageTitle': 'Proyectos',
+        'pageDescription': 'Esta es mi lista de proyectos.',
+
+        'title': 'Proyectos',
+        'path': [
+            { name: 'Home', href: '/es/' },
+            { name: 'Proyectos', href: '/es/projects' },
+        ],
+
+        'subtitle': 'Todos los Proyectos',
+        'buttonText': 'Ver proyecto'
+    },
+    'en': {
+        'pageTitle': 'Projects',
+        'pageDescription': 'This is my list of projects.',
+
+        'title': 'Projects',
+        'path': [
+            { name: 'Home', href: '/en/' },
+            { name: 'Projects', href: '/en/projects' },
+        ],
+
+        'subtitle': 'All Projects',
+        'buttonText': 'View project'
+    }
+}
