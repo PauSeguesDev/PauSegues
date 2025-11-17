1beeca1aed44e22a40e4257a6d5190d7359235c5
PaAu5 <psv.segues@gmail.com>
2025-11-17 09:35:59 +0100

    Improving minor details. Adding routes to Header

--- src/i18n/layoutTranslations.ts
+++ src/i18n/layoutTranslations.ts
@@ -1,7 +1,7 @@
 export const LayoutTranslations = {
     'ca': {
         'headerLinkTexts': ['Home', 'Sobre Mi', 'Projectes', 'Contacte'],
-        'headerLinksHref': ['/', '', '', ''],
+        'headerLinksHref': ['/', '/about-me', '/projects', '/contact'],
         
         'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         'titleSocial': "Les meves xarxes socials:",
@@ -11,7 +11,7 @@ export const LayoutTranslations = {
     },
     'es': {
         'headerLinkTexts': ['Home', 'Sobre Mi', 'Proyectos', 'Contacto'],
-        'headerLinksHref': ['/es/', '', '', ''],
+        'headerLinksHref': ['/es/', '/es/about-me', '/es/projects', '/es/contact'],
         
         'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         'titleSocial': "Mis redes sociales:",
@@ -21,7 +21,7 @@ export const LayoutTranslations = {
     },
     'en': {
         'headerLinkTexts': ['Home', 'About Me', 'Projects', 'Contact'],
-        'headerLinksHref': ['/en/', '', '', ''],
+        'headerLinksHref': ['/en/', '/en/about-me', '/en/projects', '/en/contact'],
         
         'description': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         'titleSocial': "My social networks:",
