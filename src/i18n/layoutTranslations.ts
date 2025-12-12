dc919089ee37ec4c0e77ec0c7ef8470f6d7517ce
PaAu5 <psv.segues@gmail.com>
2025-12-12 16:37:05 +0100

    feat: Implement diferents URL to diferents languages

--- src/i18n/layoutTranslations.ts
+++ src/i18n/layoutTranslations.ts
@@ -1,32 +1,29 @@
 export const LayoutTranslations = {
     'ca': {
         'headerLinkTexts': ['Home', 'Sobre Mi', 'Projectes', 'Contacte'],
-        'headerLinksHref': ['/', '/about-me', '/projects', '/contact'],
         
         'description': "Hola, sóc el Pau. Estudiant de Desenvolupament d’Aplicacions Web a Lacetània (Manresa). M’apassiona la tecnologia i la programació. M'agrada buscar i crear solucions digitals funcionals i creatives.",
         'titleSocial': "Les meves xarxes socials:",
-        'footerLegal': ["Avis Legal", "/legal"],
-        'footerPrivacity': ["Política de Privacitat", "/privacy"],
-        'footerCookies': ["Política de Cookies", "/cookies"],
+        'footerLegal': "Avis Legal",
+        'footerPrivacity': "Política de Privacitat",
+        'footerCookies': "Política de Cookies",
     },
     'es': {
         'headerLinkTexts': ['Home', 'Sobre Mi', 'Proyectos', 'Contacto'],
-        'headerLinksHref': ['/es/', '/es/about-me', '/es/projects', '/es/contact'],
         
         'description': "Hola, soy Pau. Estudiante de Desarrollo de Aplicaciones Web en Lacetània (Manresa). Me apasiona la tecnología y la programación. Me gusta buscar y crear soluciones digitales funcionales y creativas.",
         'titleSocial': "Mis redes sociales:",
-        'footerLegal': ["Aviso Legal", "/es/legal"],
-        'footerPrivacity': ["Política de privacidad", "/es/privacy"],
-        'footerCookies': ["Política de cookies", "/es/cookies"],
+        'footerLegal': "Aviso Legal",
+        'footerPrivacity': "Política de privacidad",
+        'footerCookies': "Política de cookies",
     },
     'en': {
         'headerLinkTexts': ['Home', 'About Me', 'Projects', 'Contact'],
-        'headerLinksHref': ['/en/', '/en/about-me', '/en/projects', '/en/contact'],
         
         'description': "Hi, I’m Pau. A Web Application Development student at Lacetània (Manresa). I’m passionate about technology and programming. I enjoy designing and creating functional, creative digital solutions.",
         'titleSocial': "My social networks:",
-        'footerLegal': ["Legal Notice", "/en/legal"],
-        'footerPrivacity': ["Privacy policy", "/en/privacy"],
-        'footerCookies': ["Cookie policy", "/en/cookies"],
+        'footerLegal': "Legal Notice",
+        'footerPrivacity': "Privacy policy",
+        'footerCookies': "Cookie policy",
     }
 }
---
+++ src/i18n/routes.ts
@@ -0,0 +1,37 @@
+export const routes = {
+    home: {
+        ca: '/',
+        es: '/es/',
+        en: '/en/'
+    },
+    about: {
+        ca: '/sobre-mi',
+        es: '/es/sobre-mi',
+        en: '/en/about-me'
+    },
+    projects: {
+        ca: '/projectes',
+        es: '/es/proyectos',
+        en: '/en/projects'
+    },
+    contact: {
+        ca: '/contacte',
+        es: '/es/contacto',
+        en: '/en/contact'
+    },
+    legal: {
+        ca: '/avis-legal',
+        es: '/es/aviso-legal',
+        en: '/en/legal-notice'
+    },
+    privacy: {
+        ca: '/politica-privacitat',
+        es: '/es/politica-privacidad',
+        en: '/en/privacy-policy'
+    },
+    cookies: {
+        ca: '/politica-cookies',
+        es: '/es/politica-cookies',
+        en: '/en/cookies-policy'
+    }
+}
