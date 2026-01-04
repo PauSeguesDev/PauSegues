785b29cded9eb06be6686f09ea6899e1ccecde65
Pau Segués Vitutia <pauseguesdev@gmail.com>
2026-01-04 09:21:44 +0100

    feat: add animation of Index subtitle and add in projects and index the new projectsList

--- src/i18n/indexTranslations.ts
+++ src/i18n/indexTranslations.ts
@@ -3,11 +3,16 @@ export const indexTranslations = {
         'metaTitle': 'Pau Segués Vitutia',
         'metaDescription': 'Pau Segués Vitutia Web',
 
-        'HeroSubtitle': 'Desenvolupador Web',
+        'HeroSubtitles': [
+            'Desenvolupador Web',
+            "Creador d'Aplicacions",
+            'solucions amb IA',
+            'En constant evolució 🚀'
+        ],
 
         'AboutButton': 'Sobre Mi',
         'AboutText': "Hola, sóc el Pau. Estudiant de Desenvolupament d’Aplicacions Web a Lacetània (Manresa). M’apassiona la tecnologia i la programació. M'agrada buscar i crear solucions digitals funcionals i creatives.",
-        
+
         'LastsProjectsTitle': "Últims Projectes",
         'LastsProjectsButtonText': "Veure projecte",
     },
@@ -15,11 +20,16 @@ export const indexTranslations = {
         'metaTitle': 'Pau Segués Vitutia',
         'metaDescription': 'Pau Segués Vitutia Web',
 
-        'HeroSubtitle': 'Programador Web',
+        'HeroSubtitles': [
+            'Desarrollador Web',
+            'Creador de Aplicaciones',
+            'Soluciones con IA',
+            'En constante evolución 🚀'
+        ],
 
         'AboutButton': 'Sobre Mí',
         'AboutText': 'Hola, soy Pau. Estudiante de Desarrollo de Aplicaciones Web en Lacetània (Manresa). Me apasiona la tecnología y la programación. Me gusta buscar y crear soluciones digitales funcionales y creativas.',
-    
+
         'LastsProjectsTitle': "Últimos Proyectos",
         'LastsProjectsButtonText': "Ver proyecto",
     },
@@ -27,11 +37,16 @@ export const indexTranslations = {
         'metaTitle': 'Pau Segués Vitutia',
         'metaDescription': 'Pau Segués Vitutia Web',
 
-        'HeroSubtitle': 'Web Developer',
+        'HeroSubtitles': [
+            'Web Developer',
+            'App Creator',
+            'AI Solutions',
+            'Constantly Evolving 🚀'
+        ],
 
         'AboutButton': 'About Me',
         'AboutText': 'Hi, I’m Pau. A Web Application Development student at Lacetània (Manresa). I’m passionate about technology and programming. I enjoy designing and creating functional, creative digital solutions.',
-    
+
         'LastsProjectsTitle': "Lasts Projects",
         'LastsProjectsButtonText': "See project",
     },
