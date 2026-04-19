5be250980cedfb3a4ded9143082beb4f314fa29d
Pau Segués Vitutia <pauseguesdev@gmail.com>
2026-04-19 10:40:29 +0200

    feat: add Vercel adapter to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,10 +9,11 @@ export default defineConfig({
         defaultLocale: 'ca',
         locales: ['ca', 'es', 'en'],
         routing: {
-          prefixDefaultLocale: false
+            prefixDefaultLocale: false
         }
     },
     vite: {
         plugins: [tailwindcss()]
-    }
+    },
+    adapter: vercel(),
 });
